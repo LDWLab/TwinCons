59afe79b98e6a84aed19812157d22ac2465d3e9d
Petar Penev <ppenev@gatech.edu>
2019-08-27 12:44:49 -0400

    Improving SVM plotting

--- bin/aln_SVM.py
+++ bin/aln_SVM.py
@@ -26,7 +26,9 @@ def create_and_parse_argument_options(argument_list):
     commandline_args = parser.parse_args(argument_list)
     return commandline_args, parser
 
-def load_our_data(csv_location):
+
+
+def load_csv_data(comm_args):
     '''
     Reads a csv outputted from MultiPhyMeas.py, entries starting with A_ and B_ are considered as + data
     and entries starting with C_ and D_ as - data. Normalizes all the data for x and y in the range 0,1
@@ -35,6 +37,7 @@ def load_our_data(csv_location):
     data_xy = []
     data_identity = []
     data_weights = []
+    csv_location = comm_args.csv_path
     with open(csv_location) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
@@ -51,7 +54,10 @@ def load_our_data(csv_location):
             line_count+=1
     data_xy_normx = []
     for tups in data_xy:
-        data_xy_normx.append([float(tups[0]/max(np.asarray(data_xy)[:,0])),float(tups[1]/max(np.asarray(data_xy)[:,1]))])
+        if comm_args.train:
+            data_xy_normx.append([float(tups[0]/max(np.asarray(data_xy)[:,0])),float(tups[1]/max(np.asarray(data_xy)[:,1]))])
+        elif comm_args.test:
+            data_xy_normx.append([float(tups[0])/float(comm_args.max_features[0]),float(tups[1])/float(comm_args.max_features[1])])
     return np.asarray(data_xy_normx), np.asarray(data_identity), data_weights, max(np.asarray(data_xy)[:,0]), max(np.asarray(data_xy)[:,1])
 
 def test_function(comm_args, decision_function):
@@ -65,9 +71,6 @@ def test_function(comm_args, decision_function):
         line_count = 0
         for entry in csv_reader:
             if line_count != 0:
-                #2 features
-                #test_segment = np.array([float(entry[2])/float(comm_args.max_features[0]),float(entry[3])/float(comm_args.max_features[1])])
-                #3 features
                 test_segment = np.array([float(entry[2])/float(comm_args.max_features[0]),float(entry[3])/float(comm_args.max_features[1])])
                 segment_pred = decision_function.predict(test_segment.reshape(1,-1))[0]
                 segment_dist = decision_function.decision_function(test_segment.reshape(1,-1))[0]
@@ -95,9 +98,15 @@ def mass_test(segment_pred_dist, min_threshold=0,max_threshold=2, step=0.1):
         if aln_name.split("_")[0] not in grouped_data.keys():
             grouped_data[aln_name.split("_")[0]] = []
         for segm in segment_pred_dist[aln_name]:
-            if float(segm[1][0]) == 1:
+            # Can be made so that we use a variable range to filter segments by.
+            # Eg do not consider segments with distance greater than -0.5; -0.4 etc.
+            if float (segm[1][1]) > -0.5:
                 pos_sum += 1
-                dist_sum+= segm[1][1]
+                dist_sum += abs(segm[1][1])
+                #dist_sum += segm[1][1]
+            #if float(segm[1][0]) == 1:
+            #    pos_sum += 1
+            #    dist_sum+= segm[1][1]
         grouped_data[aln_name.split("_")[0]].append(dist_sum)
 
     ###   Some plots to visualize different group results   ###
@@ -106,7 +115,7 @@ def mass_test(segment_pred_dist, min_threshold=0,max_threshold=2, step=0.1):
     #for ax, group in zip(axes.ravel(), sorted(grouped_data.keys())):
     #    ax.hist(grouped_data[group])
     #    ax.set_title(group)
-    #plt.savefig('./outputs/SVM/2ftesting_bali.png', dpi=600)
+    #plt.savefig('./outputs/SVM/2ftesting.png', dpi=600)
     #plt.clf()
 ###
     #list_for_hist = []
@@ -115,7 +124,7 @@ def mass_test(segment_pred_dist, min_threshold=0,max_threshold=2, step=0.1):
     #fig, axes = plt.subplots(1, 1)
     #axes.hist(list_for_hist, label=sorted(grouped_data.keys()),histtype='barstacked', bins=30)
     #axes.legend(prop={'size': 10})
-    #plt.savefig('./outputs/SVM/2ftesting_bali_hists.png', dpi=600)
+    #plt.savefig('./outputs/SVM/2ftesting_hists.png', dpi=600)
 
     ###   Calculate Sensitivity(tpr), Specificity(tnr) and Precision   ###
     ###   over a range of distances from the decision boundary         ###
@@ -125,10 +134,10 @@ def mass_test(segment_pred_dist, min_threshold=0,max_threshold=2, step=0.1):
         for group in grouped_data.keys():
             if group == 'A' or group == 'B':
                 fn += sum(i<thr for i in grouped_data[group])
-                tp += len(grouped_data[group])-sum(i<thr for i in grouped_data[group])
+                tp += sum(i>=thr for i in grouped_data[group])
             if group == 'C' or group == 'D':
                 tn += sum(i<thr for i in grouped_data[group])
-                fp += len(grouped_data[group])-sum(i<thr for i in grouped_data[group])
+                fp += sum(i>=thr for i in grouped_data[group])
         tpr = bypass_zero_division(tp,tp+fn)
         tnr = bypass_zero_division(tn,tn+fp)
         precision = bypass_zero_division(tp,tp+fp)
@@ -147,27 +156,32 @@ def plot_decision_function(classifier,X,y, sample_weight, axis, fig, title, deci
     Z = Z.reshape(xx.shape)
     vir_cmap = plt.cm.get_cmap('viridis')
 
-    ###   Add some comments pls   ###
+    ###   Draws each decision level (if present) with color from the viridis colormap   ###
     if decision_levels is not '':
+        # Specifies under and over values to first and last color of the colormap
         vir_cmap.set_under(vir_cmap(0))
         vir_cmap.set_over(vir_cmap(1))
         CS1 = axis.contourf(xx, yy, Z, sorted(decision_levels.keys()), 
                             colors=vir_cmap(np.linspace(0, 1, len(decision_levels)))
                             ,extend='both')
         cbar = fig.colorbar(CS1, ticks=sorted(decision_levels.keys()))
-        levels_labels = ["%.2f, %.2f, %.2f" % decision_levels[f] for f in sorted(decision_levels.keys())]
-        cbar.set_ticklabels(levels_labels)
+        levels_labels = ["%.2f, %.2f, %.2f" %decision_levels[thr] for thr in sorted(decision_levels.keys())]
+        #cbar.set_ticklabels(levels_labels)
 
+        cbar.set_ticklabels(["%.1f %s" % (thr,lev) for lev,thr in zip(levels_labels, sorted(decision_levels.keys()))])
+
+        ###   Draws a line for each decision level   ###
         #axis.contour(xx, yy, Z, colors='r', levels=[0], alpha=0.5, linestyles=['-'])
         #CS2 = axis.contour(xx, yy, Z, colors='k', levels=sorted(decision_levels.keys()),
         #                    alpha=0.5, linestyles=['-'])
         #axis.clabel(CS2, fmt='%2.1f', colors='r', fontsize=2)
+    ###   Draws the decision function as a red line   ###
     else:
-        axis.contour(xx, yy, Z, colors='r', levels=[decision_levels], alpha=0.5, linestyles=['-'])
+        axis.contour(xx, yy, Z, colors='r', levels=[0], alpha=0.5, linestyles=['-'])
 
     abs_length = [float(n)**2 for n in sample_weight]
     axis.scatter(X[:, 0], X[:, 1], c=y, s=abs_length, alpha=0.75,
-                cmap=plt.cm.bone, edgecolors='black')
+                 cmap=plt.cm.bone, edgecolors='black')#  
 
     #axis.axis('off')
     axis.set_title(title)
@@ -179,7 +193,7 @@ def main(commandline_arguments):
     ###   Load alignment segment data   ###
     X = []
     y = []
-    X, y, sample_weight, maxX, maxY = load_our_data(comm_args.csv_path)
+    X, y, sample_weight, maxX, maxY = load_csv_data(comm_args)
 
     ###   Training mode  ###
     if comm_args.train:
@@ -201,14 +215,26 @@ def main(commandline_arguments):
             raise ValueError("In test mode providing maximal values for the classifier features is required!")
         decision_function = cPickle.load(open(comm_args.test, 'rb'))
         segment_pred_dist = test_function(comm_args, decision_function)
-        dist_to_se_sp_pr = mass_test(segment_pred_dist)
+        for aln_name in sorted(segment_pred_dist.keys()):
+            print(aln_name)
+            for segment in segment_pred_dist[aln_name]:
+                if segment[1][1] > -0.5:
+                    print(segment)
+                #if segment[1][0] == 1:
+                #    print(segment)
+        dist_to_se_sp_pr = mass_test(segment_pred_dist, min_threshold=-1, max_threshold=1)
 
     ###   Plot the classifier   ###
     if comm_args.plot_df:
-        fig, axes = plt.subplots(1, 1)
-        plot_decision_function(decision_function,X,y, sample_weight, axes, fig,
+        fig, axes = plt.subplots(1, 1, )
+        if comm_args.test:
+            plot_decision_function(decision_function,X,y, sample_weight, axes, fig,
                                     "Decision function", decision_levels=dist_to_se_sp_pr)
-        plt.savefig(comm_args.plot_df, dpi=600)
+        if comm_args.train:
+            plot_decision_function(decision_function,X,y, sample_weight, axes, fig,
+                                    "Decision function")
+        plt.tight_layout()
+        plt.savefig(comm_args.plot_df, dpi=600, bbox_inches='tight')
 
 
 if __name__ == '__main__':
