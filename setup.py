d1da3ac5cc6beaee370228b9297ed0eb2b8c65e3
Petar Penev <ppenev@gatech.edu>
2020-09-02 12:40:12 -0400

    Reworking absolute max values.

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 from distutils.core import setup
+from setuptools import find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE.txt') as f:
+    license = f.read()
 
 setup(
-	name='PhyMeas',
-	version='0.1dev',
-	packages=['single_cons_comp',],
-	license='Creative Commons Attribution-Noncommercial-Share Alike license',
+	name='TwinCons',
+	version='0.2dev',
+	description='',
 	long_description=open('README.txt').read(),
+	author='Petar Penev',
+	author_email='ppenev@gatech.edu',
+	url='https://github.com/petaripenev/AlignmentScore',
+	license=license,
+	packages=find_packages(exclude=('tests', 'docs')),
 )
