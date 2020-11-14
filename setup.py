c500b04c63d5f03e8c39ffc50e3d98ac99d112cd
Petar Penev <peteripenev@gmail.com>
2020-11-14 17:02:52 -0500

    Reorganizing structure

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from distutils.core import setup
+import setuptools
 from setuptools import find_packages
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
 
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    readme = f.read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 with open(path.join(this_directory, 'LICENSE.txt'), encoding='utf-8') as f:
     license = f.read()
@@ -12,17 +12,17 @@ with open(path.join(this_directory, 'LICENSE.txt'), encoding='utf-8') as f:
 with open(path.join(this_directory, 'requirements.txt'), encoding='utf-8') as f:
     required = f.read().splitlines()
 
-setup(
-	name='TwinCons-pkg-ppenev',
-	version='0.2dev',
-	description='This projects provides several packages for analysis of MSAs comprised of two sequence groups.',
-	long_description = readme,
-	long_description_content_type='text/markdown',
-	author='Petar Penev',
-	author_email='ppenev@gatech.edu',
-	url='https://github.com/petaripenev/AlignmentScore',
-	license=license,
-	packages=find_packages(exclude=('tests', 'docs')),
-	python_requires='>=3.5',
-	install_requires=required,
+setuptools.setup(
+    name='twincons',
+    version='0.2dev',
+    description='This projects provides several packages for analysis of MSAs comprised of two sequence groups.',
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    author='Petar Penev',
+    author_email='ppenev@gatech.edu',
+    url='https://github.com/petaripenev/AlignmentScore',
+    license=license,
+    packages=find_packages(where="twincons"),
+    python_requires='>=3.5',
+    install_requires=required,
 )
